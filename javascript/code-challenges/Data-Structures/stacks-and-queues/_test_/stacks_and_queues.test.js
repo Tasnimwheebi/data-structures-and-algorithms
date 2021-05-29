2f61280eea09670cb72ee1ce5c1aac9bbb148abd
Tasnimwheebi <tasnimwheebi@gmail.com>
2021-05-30 01:40:55 +0300

    fix the test

--- javascript/code-challenges/Data-Structures/stacks-and-queues/_test_/stacks_and_queues.test.js
+++ javascript/code-challenges/Data-Structures/stacks-and-queues/_test_/stacks_and_queues.test.js
@@ -45,7 +45,7 @@ describe('queue',()=>{
     expect(node.top.value).toBe(10);
   });
 
-  it('sohuld successfully empty a stack after multiple pops',() => {
+  it('should successfully empty a stack after multiple pops',() => {
     let val = 10;
     let val2= 4;
     let node = new Stack();
@@ -55,6 +55,78 @@ describe('queue',()=>{
     node.pop();
     node.pop();
 
-    expect(node.top.value).toBeNull();
+    expect(node.sNode).toEqual(0);
   });
+  it ('should successfully instantiate an empty stack',()=>{
+    let val= 4;
+    let node = new Stack();
+    node.push(val);
+    expect(node.top.value).toBe(4);
+  });
+  it ('should successfully Call pop or peek on empty stack raises exception',()=>{
+    let node = new Stack();
+
+    function emptyStack(){
+      node.peek();
+    }
+    expect(emptyStack).toThrow();
+  });
+
+
+
+  it('should successfully enqueue multiple values into a queue',()=>{
+    let val = 10;
+    let val2= 4;
+    let val3=7;
+    let node = new Queue();
+    node.enqueue(val);
+    node.enqueue(val2);
+    node.enqueue(val3);
+    expect(node.front.value).toBe(10);
+
+  });
+  it('should successfully dequeue out of a queue the expected value',() => {
+    let val = 10;
+    let val2= 4;
+    let node = new Queue();
+
+    node.enqueue(val);
+    node.enqueue(val2);
+    node.dequeue();
+    expect(node.front.value).toBe(4);
+
+  });
+  it ('should  successfully peek into a queue, seeing the expected value',()=>{
+    let val = 10;
+    let node = new Queue();
+    node.enqueue(val);
+    node.peek();
+    expect(node.front.value).toBe(10);
+  });
+  it('should successfully empty a queue after multiple dequeues',() => {
+    let val = 10;
+    let val2= 4;
+    let node = new Queue();
+
+    node.enqueue(val);
+    node.enqueue(val2);
+    node.dequeue();
+    node.dequeue();
+    expect(node.sQueue).toBe(0);
+  });
+  it ('should successfully instantiate an empty Queue',()=>{
+    let val= 4;
+    let node = new Queue();
+    node.enqueue(val);
+    expect(node.front.value).toBe(4);
+  });
+  it ('should successfully Call pop or peek on empty queue raises exception',()=>{
+    let node = new Queue();
+
+    function emptyQueue(){
+      node.peek();
+    }
+    expect(emptyQueue).toThrow();
+  });
+
 });
--- javascript/code-challenges/Data-Structures/stacks-and-queues/queue.js
+++ javascript/code-challenges/Data-Structures/stacks-and-queues/queue.js
@@ -3,26 +3,30 @@ const Node = require('./node.js');
 class Queue {
   constructor(){
     this.front= null;
+    this.sQueue=0;
   }
   enqueue(val){
     let node = new Node(val);
     if (this.front===null){
       this.front=node;
+      this.sQueue+=1;
     }
     let current = this.front;
     while(this.front.next!==null){
       current = current.next;
+
     }
-    current = node ;
+    current.next = node ;
+    this.sQueue+=1;
   }
 
   dequeue(){
     if (this.front===null){
       throw new Error ('The Queue is empty');
     }else {
-      this.front=null;
-
-      return this.front.next;
+      let current=this.front;
+      current =this.front.next;
+      this.sQueue=this.sQueue-1;
     }
 
   }
